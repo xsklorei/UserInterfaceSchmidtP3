c31344f75d3586fda66fb02761b346d5b28fa9e6
xsklorei <jcvxka@gmail.com>
2023-04-20 11:41:31 -0700

    added script
    score text but need to update fix

--- Prototype 5/Assets/Course Library/Scripts/Target.cs
+++ Prototype 5/Assets/Course Library/Scripts/Target.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float minSpeed = 6;
+    private float maxSpeed = 10;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
-
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
     }
     Vector3 RandomForce()
@@ -39,6 +42,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
