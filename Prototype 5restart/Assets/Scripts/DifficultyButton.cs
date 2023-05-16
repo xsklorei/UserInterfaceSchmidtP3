f128d64f094654e91858b44ea212f9fbf5519f90
Jcvxka <jcvxka@gmail.com>
2023-05-15 22:14:24 -0700

    finished it all
    :)

---
+++ Prototype 5restart/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public int difficulty;
+
+    private Button button;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
